c41aa00b80fc334be09d606e6efc22d567e35f73
ddorn <diego.dorn@free.fr>
2024-06-01 05:21:37 +0200

    feat!:  Add customizable parameters and refactor font handling
    Refactor font handling and add customizable parameters for the timer.
    Removed the distinction between big and small fonts. Added command-line options for customizing bell sound, font, colors, window position, and size.
    
    BREAKING CHANGE: The `main` function now requires additional parameters for bell sound, font, colors, window position, and size.

--- pucoti.py
+++ pucoti.py
@@ -41,19 +41,9 @@ import pygame._sdl2 as sdl2
 
 
 BELL = Path(__file__).parent / "bell.mp3"
-BIG_FONT = Path(__file__).parent / "Wellbutrin.ttf"
-SMALL_FONT = BIG_FONT
-RING_INTERVAL = 20
-PURPOSE_COLOR = (183, 255, 183)
-TIMER_COLOR = (255, 224, 145)
-TEXT_TIMES_UP_COLOR = (255, 0, 0)
-TOTAL_TIME_COLOR = (183, 183, 255)
-BACKGROUND_COLOR = (0, 0, 0)
-HELP_COLOR = TIMER_COLOR
+FONT = Path(__file__).parent / "Wellbutrin.ttf"
 WINDOW_SCALE = 1.2
 POSITIONS = [(-5, -5), (5, 5), (5, -5), (-5, 5)]
-INITIAL_SIZE = (180, 70)
-PURPOSE_HISTORY_FILE = Path("~/logs/dtimer_purpose_history.jsonl").expanduser()
 SHORTCUTS = """
 J/K: -/+ 1 minute
 R: reset timer
@@ -72,9 +62,6 @@ Press any key to dismiss this message.
 """.strip()
 
 
-PURPOSE_HISTORY_FILE.parent.mkdir(parents=True, exist_ok=True)
-
-
 def fmt_time(seconds):
     if seconds < 0:
         return "-" + fmt_time(-seconds)
@@ -87,11 +74,8 @@ def fmt_time(seconds):
 
 
 @lru_cache(maxsize=40)
-def font(size: int, big: bool = True):
-    name = BIG_FONT if big else SMALL_FONT
-    f = pygame.font.Font(name, size)
-    # f.align = FONT_CENTER
-    return f
+def get_font(size: int):
+    return pygame.font.Font(FONT, size)
 
 
 @lru_cache(maxsize=100)
@@ -99,28 +83,27 @@ def text(
     text: str,
     size: int | tuple[int, int],
     color: tuple,
-    big: bool = True,
     monospaced_time: bool = False,
 ):
     if not isinstance(size, int):
         if monospaced_time:
             # Use the font size that fits a text equivalent to the time.
             # We use 0s to make sure the text is as wide as possible and doesn't jitter.
-            size = auto_size(re.sub(r"\d", "0", text), size, big)
+            size = auto_size(re.sub(r"\d", "0", text), size)
         else:
-            size = auto_size(text, size, big)
+            size = auto_size(text, size)
 
     if not monospaced_time:
-        return font(size, big).render(text, True, color)
+        return get_font(size).render(text, True, color)
     else:
         digits = "0123456789"
         # We render each char independently to make sure they are monospaced.
-        chars = [font(size, big).render(c, True, color) for c in text]
+        chars = [get_font(size).render(c, True, color) for c in text]
         # Make each digit the size of a 0.
-        width = font(size, big).size("0")[0]
+        width = get_font(size).size("0")[0]
         full_width = sum(s.get_width() if c not in digits else width for c, s in zip(text, chars))
         # Create a surface with the correct width.
-        surf = pygame.Surface((full_width, chars[0].get_height()))
+        surf = pygame.Surface((full_width, chars[0].get_height()), SRCALPHA)
         # Blit each char at the correct position.
         x = 0
         for c, s in zip(text, chars):
@@ -133,14 +116,14 @@ def text(
         return surf
 
 
-def size_with_newlines(text: str, size: int, big: bool = True):
+def size_with_newlines(text: str, size: int):
     """Return the size of the text with newlines."""
     lines = text.split("\n")
-    line_height = font(size, big).get_linesize()
-    return (max(font(size, big).size(line)[0] for line in lines), len(lines) * line_height)
+    line_height = get_font(size).get_linesize()
+    return (max(get_font(size).size(line)[0] for line in lines), len(lines) * line_height)
 
 
-def auto_size(text: str, max_rect: tuple[int, int], big_font: bool = True):
+def auto_size(text: str, max_rect: tuple[int, int]):
     """Find the largest font size that will fit text in max_rect."""
     # Use dichotomy to find the largest font size that will fit text in max_rect.
 
@@ -148,7 +131,7 @@ def auto_size(text: str, max_rect: tuple[int, int], big_font: bool = True):
     max_size = max_rect[1]
     while min_size < max_size:
         font_size = (min_size + max_size) // 2
-        text_size = size_with_newlines(text, font_size, big_font)
+        text_size = size_with_newlines(text, font_size)
 
         if text_size[0] <= max_rect[0] and text_size[1] <= max_rect[1]:
             min_size = font_size + 1
@@ -245,19 +228,40 @@ app = typer.Typer(add_completion=False)
 
 
 @app.command()
-def main(initial_timer: list[str]):
+def main(
+    initial_timer: list[str],
+    bell: Annotated[Path, "Path to the bell sound file."] = BELL,
+    ring_every: int = 20,
+    # ring_count: int = -1,
+    font: Annotated[Path, "Path to the font for all the text."] = FONT,
+    background_color: tuple[int, int, int] = (0, 0, 0),
+    timer_color: tuple[int, int, int] = (255, 224, 145),
+    timer_up_color: tuple[int, int, int] = (255, 0, 0),
+    purpose_color: tuple[int, int, int] = (183, 255, 183),
+    total_time_color: tuple[int, int, int] = (183, 183, 255),
+    window_position: tuple[int, int] = (-5, -5),
+    window_size: tuple[int, int] = (180, 70),
+    history_file: Annotated[Path, "Path to the file where the purpose history is stored."] = Path(
+        "~/.pucoti_history"
+    ),
+) -> None:
     """
     DTimer is simple but flexible countdown timer.
 
     Help is available by pressing h or ?.
     """
 
+    global FONT
+    FONT = font
+    history_file = history_file.expanduser()
+    history_file.parent.mkdir(parents=True, exist_ok=True)
+
     pygame.init()
     pygame.mixer.init()
     pygame.key.set_repeat(300, 20)
 
     pygame.print_debug_info()
-    window = sdl2.Window("DTimer", INITIAL_SIZE, borderless=True, always_on_top=True)
+    window = sdl2.Window("DTimer", window_size, borderless=True, always_on_top=True)
     window.get_surface().fill((0, 0, 0))
     window.flip()
 
@@ -265,7 +269,7 @@ def main(initial_timer: list[str]):
     clock = pygame.time.Clock()
 
     position = 0
-    place_window(window, *POSITIONS[position])
+    place_window(window, *window_position)
 
     initial_duration = human_duration(*initial_timer)
     start = time()
@@ -327,26 +331,26 @@ def main(initial_timer: list[str]):
         if last_scene == Scene.ENTERING_PURPOSE and scene != last_scene:
             if purpose and (not purpose_history or purpose != purpose_history[-1].text):
                 purpose_history.append(Purpose(purpose, time()))
-                with PURPOSE_HISTORY_FILE.open("a") as f:
+                with history_file.open("a") as f:
                     f.write(json.dumps(purpose_history[-1].__dict__) + "\n")
 
-        screen.fill(BACKGROUND_COLOR)
+        screen.fill(background_color)
 
         # Render purpose, if there is space.
         if purpose_rect := layout.get("purpose"):
-            t = text(purpose, purpose_rect.size, PURPOSE_COLOR)
+            t = text(purpose, purpose_rect.size, purpose_color)
             r = screen.blit(t, t.get_rect(center=purpose_rect.center))
             if scene == Scene.ENTERING_PURPOSE and (time() % 1) < 0.7:
                 if r.height == 0:
                     r.height = purpose_rect.height
                 if r.right >= purpose_rect.right:
                     r.right = purpose_rect.right - 3
-                pygame.draw.line(screen, PURPOSE_COLOR, r.topright, r.bottomright, 2)
+                pygame.draw.line(screen, purpose_color, r.topright, r.bottomright, 2)
 
         # Render time.
         if time_rect := layout.get("time"):
             remaining = timer - (time() - start)
-            color = TEXT_TIMES_UP_COLOR if remaining < 0 else TIMER_COLOR
+            color = timer_up_color if remaining < 0 else timer_color
             t = text(fmt_time(abs(remaining)), time_rect.size, color, monospaced_time=True)
             screen.blit(t, t.get_rect(center=time_rect.center))
 
@@ -354,25 +358,25 @@ def main(initial_timer: list[str]):
             t = text(
                 fmt_time(time() - start),
                 total_time_rect.size,
-                TOTAL_TIME_COLOR,
+                total_time_color,
                 monospaced_time=True,
             )
             screen.blit(t, t.get_rect(center=total_time_rect.center))
 
         if help_rect := layout.get("help"):
-            screen.fill(BACKGROUND_COLOR)
-            t = text(HELP, help_rect.size, HELP_COLOR, big=False)
+            screen.fill(background_color)
+            t = text(HELP, help_rect.size, timer_color)
             screen.blit(t, t.get_rect(center=help_rect.center))
 
         if purpose_history_rect := layout.get("purpose_history"):
-            screen.fill(BACKGROUND_COLOR)
+            screen.fill(background_color)
             t = "\n".join(f"({fmt_time(p.timestamp - start)}) {p.text}" for p in purpose_history)
-            t = text("HISTORY\n" + t, purpose_history_rect.size, PURPOSE_COLOR, big=False)
+            t = text("HISTORY\n" + t, purpose_history_rect.size, purpose_color)
             screen.blit(t, t.get_rect(center=purpose_history_rect.center))
 
         # Ring the bell if the time is up.
-        if remaining < 0 and time() - last_rung > RING_INTERVAL:
-            play(BELL)
+        if remaining < 0 and time() - last_rung > ring_every:
+            play(bell)
             last_rung = time()
 
         window.flip()
